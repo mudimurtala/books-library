0359d405cd6330a00197f80324e01f804b5857ef
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-20 17:02:23 +0100

    Add files

--- App.jsx
+++ App.jsx
@@ -2,7 +2,9 @@ import Header from "./components/Header"
 
 export default function App() {
     return (
-        <h3>Dance</h3>
-        <Header />
+        <>
+            <h3>Dance</h3>
+            <Header />
+        </>
     )
 }
