19f3964dfc046c2bf0bc6c4885c76e60c873ce16
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-20 17:20:31 +0100

    Add files

--- App.jsx
+++ App.jsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header"
+import Entry from "./components/Entry"
+import Footer from "./components/Footer"
 
 export default function App() {
     return (
         <>
             <h3>Dance</h3>
             <Header />
+            <Entry />
+            <Footer />
         </>
     )
 }
