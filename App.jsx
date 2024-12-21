cab5c41ff67740e270aa231dafb80083f43b15a8
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-21 14:41:45 +0100

    Add files

--- App.jsx
+++ App.jsx
@@ -1,11 +1,15 @@
 import Header from "./components/Header"
 import Entry from "./components/Entry"
-import Footer from "./components/Footer"
+// import Footer from "./components/Footer"
 
 export default function App() {
     return (
         <>
             <Header />
+            <main className="container">
+
+            </main>
+            {/* <Footer /> */}
         </>
     )
 }
