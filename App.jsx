eec49d8d1c0f222d3993c6b6762e72ef0b610d06
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-21 15:04:23 +0100

    Add files

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header"
 import Entry from "./components/Entry"
 // import Footer from "./components/Footer"
+import data from "./data"
 
 export default function App() {
     return (
