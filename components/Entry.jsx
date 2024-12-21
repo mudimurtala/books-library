d5bcf641b9f11141990fdc9ac6b0cf0e7630372b
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-21 17:47:47 +0100

    Add files

--- components/Entry.jsx
+++ components/Entry.jsx
@@ -1,15 +1,23 @@
+import category from "/images/category.png"
+
 export default function Entry(props) {
     return (
         <article className="book-entry">
             <div className="main-image-container">
                 <img
-                    className="main-image" 
+                    className="main-image"
                     src={props.img.src}
                     alt={props.img.alt}
                 />
             </div>
             <div className="info-container">
-
+                <img
+                    className="category"
+                    src={category}
+                    alt="category icon"
+                />
+                <span className="genre">{props.genre}</span>
+                <a href={props.linkToPDF} target="_blank">link to PDF</a>
             </div>
         </article>
     )
