3d1876b23d50389b219bf1b294e50077edb2c797
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-21 13:50:21 +0100

    Add files

--- data.js
+++ data.js
@@ -9,6 +9,6 @@ export default [
         genre: "Productivity",
         linkToPDF: "https://drive.google.com/file/d/1l_L40XGeueuzlWPTc8eApuHj9CgwZAIR/view?usp=drive_link",
         authorAndYear: "James Clear - 2018",
-        description: ""
+        description: "James Clear's *Atomic Habits* explores how small, consistent habits can lead to remarkable personal and professional transformations. It offers practical strategies for building good habits, breaking bad ones, and mastering the tiny changes that compound into significant growth over time."
     },
 ]
