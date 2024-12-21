feededea1dda421f2ef62b3846aff8ced27231ef
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-21 10:58:12 +0100

    Add files

--- data.js
+++ data.js
@@ -1,5 +1,9 @@
 export default [
     {
-        
+        id: 1,
+        img: {
+            src: "https://unsplash.com/photos/a-stack-of-books-with-a-white-object-on-top-of-it-l98dIPnmIgQ",
+            alt: "Atomic Habits"
+        }
     },
 ]
