0afb4c837a70a890eae4827ccf76039a986981d3
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-22 06:28:42 +0100

    Add files

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer>
-                <h1>mudi murtala</h1>
+                <h1>created by <a href="https://www.linkedin.com/in/mudimurtala/" target="_blank">mudi</a></h1>
         </footer>
     )
 }
