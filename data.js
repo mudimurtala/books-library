39f54a9aebd6a76e9f04217ae20067c43c4b8e10
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-22 07:35:44 +0100

    Add files

--- data.js
+++ data.js
@@ -26,14 +26,14 @@ export default [
     {
         id: 3,
         img: {
-            src: "public/images/imagebook.avif",
-            alt: "Atomic Habits"
+            src: "public/images/think-and-grow-rich-.jpg",
+            alt: "Think and Grow Rich"
         },
-        title: "Atomic Habits",
-        genre: "Productivity",
-        linkToPDF: "https://drive.google.com/file/d/1l_L40XGeueuzlWPTc8eApuHj9CgwZAIR/view?usp=drive_link",
-        authorAndYear: "James Clear - 2018",
-        description: "James Clear's Atomic Habits explores how small, consistent habits can lead to remarkable personal and professional transformations. It offers practical strategies for building good habits, breaking bad ones, and mastering the tiny changes that compound into significant growth over time."
+        title: "Think and Grow Rich",
+        genre: "Personal Development",
+        linkToPDF: "https://drive.google.com/file/d/1ciNS9z_9tiUOF_iG1AeNCPQaxBHG7b0m/view?usp=drive_link",
+        authorAndYear: "Napoleon Hill - 2000",
+        description: "Think and Grow Rich by Napoleon Hill is a classic self-help book that outlines principles for achieving success, drawing from the experiences of influential individuals like Andrew Carnegie and Henry Ford. It emphasizes the power of desire, faith, persistence, and the subconscious mind to turn goals into reality, offering a step-by-step formula for wealth and personal achievement."
     },
     {
         id: 4,
