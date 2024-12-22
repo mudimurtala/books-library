4c1aa7416dacb0312da03d6fb31e179c2aaa0217
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-22 07:08:06 +0100

    Add files

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ export default [
     {
         id: 1,
         img: {
-            src: "public/images/imagebook.avif",
+            src: "public/images/atomichabits.jpeg",
             alt: "Atomic Habits"
         },
         title: "Atomic Habits",
@@ -14,14 +14,14 @@ export default [
     {
         id: 2,
         img: {
-            src: "public/images/imagebook.avif",
-            alt: "Atomic Habits"
+            src: "public/images/richdad.jpg",
+            alt: "Rich Dad Poor Dad"
         },
-        title: "Atomic Habits",
-        genre: "Productivity",
-        linkToPDF: "https://drive.google.com/file/d/1l_L40XGeueuzlWPTc8eApuHj9CgwZAIR/view?usp=drive_link",
-        authorAndYear: "James Clear - 2018",
-        description: "James Clear's Atomic Habits explores how small, consistent habits can lead to remarkable personal and professional transformations. It offers practical strategies for building good habits, breaking bad ones, and mastering the tiny changes that compound into significant growth over time."
+        title: "Rich Dad, Poor Dad",
+        genre: "Financial Freedom",
+        linkToPDF: "https://drive.google.com/file/d/1vooHyMyLl1sG3-_CSiIqVpn4POVAMth6/view?usp=drive_link",
+        authorAndYear: "Robert T. Kiyosaki - 2011",
+        description: "Rich Dad, Poor Dad by Robert T. Kiyosaki contrasts the financial philosophies of the author’s two father figures: his wealthy mentor ('Rich Dad') and his biological father ('Poor Dad'). It teaches principles like asset-building, financial literacy, and entrepreneurship, emphasizing how mindset and education shape financial success."
     },
     {
         id: 3,
